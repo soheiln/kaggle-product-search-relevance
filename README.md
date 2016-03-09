49857c6cdac48906085f5bce610ff661fa127500
Soheil Norouzi <soheil.nrz@gmail.com>
2016-03-08 18:43:43 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kaggle-product-search-relevance
---
+++ analyze.py
@@ -0,0 +1,42 @@
+# project intro
+"""
+The goal of this project is to learn from home depot product search relevance data and
+create a model to predict/score the relevance of pairs of search queries and products
+"""
+
+# imports
+import numpy as np
+import pylab as pl
+import nltk
+from sklearn.tree import DecisionTreeRegressor
+from sklearn import metrics
+from sklearn.cross_validation import train_test_split
+from sklearn.grid_search import GridSearchCV
+
+# read and pre-process input data
+def read_input_data():
+  pass
+
+# create word dictionary
+def create_word_dictionary():
+  pass
+
+# transform input data create new feature set in the form of an input matrix
+def transform_input_data():
+  pass
+
+# randomly run split data into train and validation sets
+
+# try SVM and DT regressors
+
+# analyze performance
+
+# pick new regressor and optimize hyper parameters
+
+# create final report
+
+
+def main():
+  pass
+
+main()
